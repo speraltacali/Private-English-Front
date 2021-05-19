6fce296e054acd8843e701f170797bfde6b12b45
Santiago Agustin Peralta Cali <santiagoperaltacali@hotmail.com>
2021-05-19 02:03:05 -0300

    subir img firebase

--- src/app/Shared/Upload-Image/Helpers/imageValidator.ts
+++ src/app/Shared/Upload-Image/Helpers/imageValidator.ts
@@ -4,8 +4,9 @@ import { FileItem } from '../Models/file-item';
 export class ImageValidator{
     private acceptType = ['image/jpeg' , 'image/png'];
 
-    validateType(fileType: string):boolean{
-        return fileType == '' || fileType == undefined
+    validateType(fileType: string): boolean{
+
+        return fileType === '' || fileType === undefined
         ? false
         : this.acceptType.includes(fileType); 
     }
